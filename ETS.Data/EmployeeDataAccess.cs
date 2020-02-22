a2197471c531d64ca36a2b90b8e5b7f621ae446f
Anya <57970138+annatnguyen84@users.noreply.github.com>
2020-02-22 23:23:23 +1100

    update

--- ETS.Data/EmployeeDataAccess.cs
+++ ETS.Data/EmployeeDataAccess.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
 using System.Data.SqlClient; // STEP 1: Select the SQL provider
 using ETS.Entity; // use Employee
 using System.Data;
